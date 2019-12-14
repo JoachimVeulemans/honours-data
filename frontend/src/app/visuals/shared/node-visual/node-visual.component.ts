fc334b0e78e79fbf18bfe83709bb3d74948320f3
Joachim Veulemans <joachim.veulemans@outlook.be>
2019-12-14 23:44:14 +0100

    Fit more text in the nodes

--- frontend/src/app/visuals/shared/node-visual/node-visual.component.ts
+++ frontend/src/app/visuals/shared/node-visual/node-visual.component.ts
@@ -4,19 +4,42 @@ import { Node } from '../../../d3/models/node';
 @Component({
   selector: '[nodeVisual]',
   template: `
-    <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
-      <svg:circle
+    <svg:g [attr.transform]="'translate(' + _node.x + ',' + _node.y + ')'">
+      <svg:ellipse
           cx="0"
           cy="0"
           r="40"
+          rx="100"
+          ry="50"
           stroke="black" stroke-width="3" fill="pink">
-      </svg:circle>
-      <svg:text text-anchor="middle">
-        {{node.id}}
-      </svg:text>
+      </svg:ellipse>
+      <svg:text x="0" y="-10" text-anchor="middle">
+        {{_node.id}}
+        <tspan x="0" y="10">{{_text2}}</tspan>
+        <tspan x="0" y="30">{{_text3}}</tspan>
+        </svg:text>
     </svg:g>
   `
 })
 export class NodeVisualComponent {
-  @Input('nodeVisual') node: Node;
+  private _node: Node;
+  private _text2: string = '';
+  private _text3: string = '';
+  @Input('nodeVisual')
+  set nodeVisual(value: Node) {
+    this._node = value;
+    const words = this._node.id.split(" ");
+    this._node.id = '';
+    this._text2 = '';
+
+    for(let i = 0; i < words.length; i++) {
+      if (i < 4) {
+        this._node.id += words[i] + " ";
+      } else if (i < 10) {
+        this._text2 += words[i] + " ";
+      } else {
+        this._text3 += words[i] + " ";
+      }
+    }
+  }
 }
