4dc263e6d175599444dac437417689da9dbc9053
Joachim Veulemans <joachim.veulemans@outlook.be>
2019-12-14 14:38:43 +0100

    Added custom classes

---
+++ frontend/src/app/models/bigtree.model.ts
@@ -0,0 +1,11 @@
+import { UnityPosition } from './unityposition.model';
+
+export class BigTree {
+    public ChildIdeaDescriptions: string[];
+    public Description: string;
+    public GameObjectName: string;
+    public Position: UnityPosition;
+    public Votes: number;
+
+    constructor() { }
+}
